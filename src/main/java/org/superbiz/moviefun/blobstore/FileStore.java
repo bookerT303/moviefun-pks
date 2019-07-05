a49988c3e709e5ee8ff64c8fb4e1bf0f96f68d55
Brad Davidson <bdavidson@pivotal.io>
2019-07-05 14:56:47 -0600

    PersistentVolume attempt failed

--- src/main/java/org/superbiz/moviefun/blobstore/FileStore.java
+++ src/main/java/org/superbiz/moviefun/blobstore/FileStore.java
@@ -1,7 +1,9 @@
 package org.superbiz.moviefun.blobstore;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.tika.Tika;
 import org.apache.tika.io.IOUtils;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,16 +15,22 @@ import java.util.Optional;
 public class FileStore implements BlobStore {
 
     private final Tika tika = new Tika();
+    private String albumRootDirectory;
 
+    public FileStore(@Value("${moviefun.storage.directory:}") String albumRootDirectory) {
+        this.albumRootDirectory = albumRootDirectory;
+        if (StringUtils.isNotEmpty(albumRootDirectory)) {
+            new File(albumRootDirectory).mkdirs();
+        }
+    }
 
     @Override
     public void put(Blob blob) throws IOException {
-        File targetFile = new File(blob.getName());
+        File targetFile = new File(albumRootDirectory, blob.getName());
 
         targetFile.delete();
         targetFile.getParentFile().mkdirs();
         targetFile.createNewFile();
-
 
         try (FileOutputStream outputStream = new FileOutputStream(targetFile)) {
             IOUtils.copy(blob.getInputStream(), outputStream);
@@ -31,7 +39,7 @@ public class FileStore implements BlobStore {
 
     @Override
     public Optional<Blob> get(String name) throws IOException {
-        File file = new File(name);
+        File file = new File(albumRootDirectory, name);
 
         if (!file.exists()) {
             return Optional.empty();
